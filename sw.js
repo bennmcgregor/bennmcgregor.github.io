c41c2553d09549797bb9cc6c5f3c4f81aa197da8
Benn McGregor <benn@Benns-MacBook-Pro.local>
2019-07-09 18:36:56 -0400

    made some edits

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-51258c3300183fa603d9.js"
+    "url": "webpack-runtime-9517e1c3522bbe967905.js"
   },
   {
     "url": "app-e09099c19b7e12ebee35.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "632306540edc71b8d0ab1ab3fe8e8570"
+    "revision": "546c2a66c822aac0bec8be490c0296a5"
   },
   {
     "url": "component---src-pages-404-js.8bcfda05a26b1582af4a.css"
